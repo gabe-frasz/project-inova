eecb309b391cc92557500f8a6e262fc338a4dbe1
Gabriel <gabrielvitor.frasao@gmail.com>
2022-06-09 18:16:04 -0300

    state moved to cards component

--- refactored-with-next/components/widgets/Button/button.tsx
+++ refactored-with-next/components/widgets/Button/button.tsx
@@ -1,20 +1,23 @@
 import { ButtonType } from "@/types";
-import { useState } from "react";
 
-export const Button = ({ variant, reverse, customClass }: ButtonType) => {
-  const [toggleButton, setToggleButton] = useState(false);
-
+export const Button = ({
+  variant,
+  reverse,
+  toggleButton,
+  showMore,
+  customClass,
+}: ButtonType) => {
   if (variant === "view-more") {
     return (
       <button
-        onClick={() => setToggleButton(!toggleButton)}
+        onClick={toggleButton}
         className={`w-60 py-2 ${
           reverse
             ? "bg-white text-black hover:text-brand-500"
             : "bg-brand-500 text-white hover:bg-white hover:text-brand-500"
         } text-sm lg:text-base rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-xl ${customClass}`}
       >
-        {toggleButton ? "Ver mais serviços" : "Ver menos"}
+        {showMore ? "Ver menos" : "Veja mais serviços"}
       </button>
     );
   }
@@ -27,7 +30,7 @@ export const Button = ({ variant, reverse, customClass }: ButtonType) => {
           : "bg-brand-500 text-white hover:bg-white hover:text-brand-500"
       } text-sm lg:text-base rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-xl ${customClass}`}
     >
-      {variant == "budget" ? "Solicitar orçamento" : "Ver mais serviços"}
+      {variant == "budget" ? "Solicitar orçamento" : "Veja mais serviços"}
     </button>
   );
 };
--- refactored-with-next/core/types/button-type.ts
+++ refactored-with-next/core/types/button-type.ts
@@ -1,5 +1,7 @@
 export type ButtonType = {
   variant: "budget" | "view-more";
   reverse?: boolean;
+  toggleButton?: () => void;
+  showMore?: boolean;
   customClass?: string;
 };
