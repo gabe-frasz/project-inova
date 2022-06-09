38c6afda6ea5a6100f6a0d9dfeb4fbfb61348069
Gabriel <gabrielvitor.frasao@gmail.com>
2022-06-09 19:01:27 -0300

    layout finished

--- refactored-with-next/pages/sobre/index.tsx
+++ refactored-with-next/pages/sobre/index.tsx
@@ -1,7 +1,18 @@
+import { Budget, Content } from "@components/modules";
 import type { NextPage } from "next";
 
 const About: NextPage = () => {
-  return <div>Sobre</div>;
+  return (
+    <div className="flex-1 flex flex-col bg-brand-500 text-white text-center">
+      <div className="flex-1 container mx-auto px-4 py-16 flex flex-col justify-center items-center">
+        <Content variant="about" />
+
+        <hr className="w-full my-16 border-t-2 opacity-50" />
+
+        <Budget />
+      </div>
+    </div>
+  );
 };
 
 export default About;
