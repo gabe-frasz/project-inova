a70ee3607639af7a30baaef59dc685e70f3758f9
Gabriel <gabrielvitor.frasao@gmail.com>
2022-11-25 18:18:43 -0300

    refactor: minor content adjustments

--- core/utils/services-info.ts
+++ core/utils/services-info.ts
@@ -10,42 +10,35 @@ export const servicesInfo: ServiceInfo[] = [
     id: 1,
     title: "Alpinismo industrial",
     description:
-      "Alpinismo industrial é uma técnica largamente utilizada na execução de trabalhos em altura. Seja na indústria, na construção civil, na mineração ou mesmo em serviços de limpeza e manutenção, seu principal objetivo é viabilizar o trabalho em locais de difícil alcance.",
+      "Alpinismo industrial é uma técnica largamente utilizada na execução de trabalhos em altura. Seja na indústria ou mesmo em serviços de limpeza e manutenção, seu principal objetivo é viabilizar o trabalho em locais de difícil alcance.",
     imageUrl: "/images/service-alpinismo-industrial.png",
   },
   {
     id: 2,
     title: "Pintura industrial",
     description:
-      "Pintura industrial é uma técnica de proteção anticorrosiva utilizada para a proteção de superfícies metálicas. Sua aplicação é feita em setores como indústrias siderúrgicas, petrolíferas, construção civil, entre outros…",
+      "Pintura industrial é uma técnica de proteção anticorrosiva utilizada para a proteção de superfícies metálicas. Sua aplicação é feita em setores como indústrias siderúrgicas, petrolíferas, construção civil, entre outros.",
     imageUrl: "/images/service-pintura-industrial.png",
   },
   {
     id: 3,
     title: "Restauração de fachada predial",
     description:
-      "A restauração de fachada predial trata do desgaste que as fachadas dos edifícios sofrem com o decorrer do tempo. Por estarem expostos constantemente às variadas condições climáticas, os materiais ou pinturas que compõem a fachada de um prédio sofrem desgaste, trincam e perdem a beleza inicial.",
+      "A restauração de fachada predial trata do desgaste que as fachadas dos edifícios sofrem com o decorrer do tempo, por estarem expostos constantemente às variadas condições climáticas.",
     imageUrl: "/images/service-restauracao-de-fachada-predial.png",
   },
   {
     id: 4,
     title: "SPDA",
     description:
-      "Um Sistema de Proteção contra Descargas Atmosféricas (SPDA) tem por objetivo principal evitar a incidência direta de raios na estrutura a ser protegida, através da constituição de pontos preferenciais de incidência para as descargas que eventualmente atingiriam a estrutura na ausência do sistema.",
-    imageUrl: "/images/service-spda-e-manutencao-de-sistema-fotovoltaico.png",
-  },
-  {
-    id: 5,
-    title: "Manutenção de sistema fotovoltaico",
-    description:
-      "A manutenção preditiva do sistema de energia solar consiste no monitoramento da sua geração para possíveis quedas de desempenho e na inspeção visual dos equipamentos, incluindo o painel solar, inversor e string box, a fim de detectar precocemente indícios de danos, como arranhões, manchas ou rachaduras nas placas solares. Já a manutenção preventiva do sistema de energia solar engloba a limpeza periódica das placas solares, a higienização do inversor e uma inspeção completa de todos os componentes elétricos (módulos, inversor, string box e conectores) e mecânicos (suportes e estrutura de fixação) para otimizar o desempenho, identificar/reparar danos e prolongar a vida útil dos equipamentos.",
+      "Um Sistema de Proteção contra Descargas Atmosféricas (SPDA) tem por objetivo principal evitar a incidência direta de raios na estrutura a ser protegida, através da constituição de pontos preferenciais de incidência para as descargas.",
     imageUrl: "/images/service-spda-e-manutencao-de-sistema-fotovoltaico.png",
   },
   {
     id: 6,
     title: "Limpeza de fachadas",
     description:
-      "A limpeza de fachada predial é uma tarefa que envolve uma série de conhecimentos específicos, do tipo ideal de detergente a técnicas de alpinismo e normas de segurança, para que possa ser realizada de maneira eficiente e segura. Por isso são necessários profissionais capacitados e experientes. Já a limpeza de fachada refere-se ao processo de garantir que o exterior de um edifício esteja livre de qualquer sujeira, fuligem, manchas e poluentes. Existem várias razões pelas quais a fachada de um edifício pode parecer suja: impacto do clima, pássaros, poluentes no ar, ação dos moradores, entre várias outras.",
+      "A limpeza de fachada predial é uma tarefa que envolve uma série de conhecimentos específicos, do tipo ideal de detergente a técnicas de alpinismo e normas de segurança, para ser realizada de maneira eficiente e segura. Por isso são necessários profissionais capacitados e experientes. Já a limpeza de fachada refere-se ao processo de garantir que o exterior de um edifício esteja livre de qualquer sujeira.",
     imageUrl: "/images/service-limpeza-de-fachadas.png",
   },
   {
@@ -60,15 +53,22 @@ export const servicesInfo: ServiceInfo[] = [
     id: 8,
     title: "Trabalho em espaço confinado",
     description:
-      "Espaços confinados basicamente são todos os locais que não foram projetados para a permanência humana, apresentando, em sua grande maioria, limitados meios de entrada, saída e mobilidade em geral.",
+      "A Linha de Vida é um equipamento fundamental para a segurança dos colaboradores que atuam no Trabalho em Altura. Isso porque permite que o trabalhador se locomova em segurança mesmo que esteja a mais de dois metros de altura do chão.",
     imageUrl: "/images/service-trabalho-em-espaco-confinado.png",
   },
   {
     id: 9,
     title: "Limpeza de caixa d'água",
     description:
-      "Você sabia que deve fazer a limpeza da caixa d'água a cada seis meses? O reservatório de água das casas precisa ser higienizado para evitar a contaminação e surgimento de bactérias que afetam diretamente a saúde da família, além de prevenir imprevistos com encanamentos entupidos.",
+      "Você sabia que deve fazer a limpeza da caixa d'água a cada seis meses? O reservatório de água das casas precisa ser higienizado para evitar a contaminação e surgimento de bactérias que afetam diretamente a saúde da família.",
     imageUrl: "/images/service-limpeza-de-caixa-dagua.png",
+  },
+  {
+    id: 5,
+    title: "Manutenção de sistema fotovoltaico",
+    description:
+      "A manutenção preditiva do sistema de energia solar consiste no monitoramento da sua geração para possíveis quedas de desempenho e na inspeção visual dos equipamentos, incluindo o painel solar, inversor e string box, a fim de detectar defeitos nas placas solares. Já a manutenção preventiva do sistema de energia solar engloba a limpeza periódica das placas solares, a higienização do inversor e uma inspeção completa de todos os componentes elétricos e mecânicos para otimizar o desempenho.",
+    imageUrl: "/images/service-spda-e-manutencao-de-sistema-fotovoltaico.png",
   },
   {
     id: 10,
